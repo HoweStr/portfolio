5201578ea51d12f3185b93c5f4c6d457d214e8cf
HoweStr <imedia@imd-im-60a.camp.confederationc.on.ca>
2019-04-16 10:53:28 -0400

    changes
    readme
    images
    style

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # portfolio
 - hello world
+
+https://www.linkedin.com/in/phil-howie-strang/
+
+https://colorlib.com/wp/template/po-portfolio/
+
+https://www.youtube.com/watch?v=1kpXcQddXZU&t=346s - 3DS Max lighting tutorial
+https://www.youtube.com/watch?v=gILmtT_osx0&t=563s - 3DS Max lighting tutorial 2
+https://www.youtube.com/watch?v=0lZ-CImMTrA&t=867s - 3DS Max - bedroom tutorial modelling 
--- js/custom.js
+++ js/custom.js
@@ -24,11 +24,11 @@ var modal = document.getElementById('myModal');
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  
 }
 
 // Get the <span> element that closes the modal
@@ -38,3 +38,4 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
